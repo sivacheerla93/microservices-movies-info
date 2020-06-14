596611471b9ae5d08157b0f1ce2b2e184189f640
Siva Cheerla <sivacheerla@live.com>
2020-06-14 12:15:08 +0530

    14/06/20

--- movies-info-search/src/main/java/com/siva/moviesinfosearch/controllers/MoviesInfoSearchController.java
+++ movies-info-search/src/main/java/com/siva/moviesinfosearch/controllers/MoviesInfoSearchController.java
@@ -1,14 +1,36 @@
 package com.siva.moviesinfosearch.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import com.siva.moviesinfosearch.models.SearchResponse;
 
 @RestController
+@RequestMapping(value = "/search")
 public class MoviesInfoSearchController {
 
-	@GetMapping(value = "/")
+	@Value("${api.key}")
+	private String apiKey;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@RequestMapping(value = "")
 	public String getHello() {
 		return "This is Movies info search service!";
+	}
+
+	@RequestMapping(value = "/{query}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public SearchResponse getSearchReponse(@PathVariable("query") String query) {
+		SearchResponse searchResponse = restTemplate
+				.getForObject("https://api.themoviedb.org/3/search/movie?language=en-US&include_adult=false&api_key="
+						+ apiKey + "&query=" + query, SearchResponse.class);
+		return searchResponse;
 	}
 
 }
